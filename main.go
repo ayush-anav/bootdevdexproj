6a4b436223fc1f495afbddd9b809d0ca6638922e
Ayush Sharma <ayushanavsharma2@gmail.com>
2026-03-19 14:13:03 +1200

    2. added ability to now capture user input and display the first word back in the prompt

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
-package main 
-import "fmt"
+package main
 
-func main(){
-	fmt.Println("Hello World")
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// creating a way to get user input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		// block for user input
+		scanner.Scan()
+		userInput := scanner.Text()
+		cleanedText := cleanInput(userInput)
+		fmt.Printf("Your command was: %s\n", cleanedText[0])
+	}
+
 }
